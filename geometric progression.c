599e085ddd98f2b34ffdea3edec01c05638e19f2
Susmithakothala <84321100+Susmithakothala@users.noreply.github.com>
2021-08-17 20:12:34 +0530

    Add files via upload

---
+++ geometric progression.c
@@ -0,0 +1,22 @@
+# include <stdio .h>
+# include <math .h>
+int main ()
+{
+int n, x, i, sum = 0;
+printf ("\ nGEOMETRIC PROGRESSION ");
+printf ("\n Enter the limit \n");
+scanf ("%d", &n);
+printf (" Enter the value of x\n");
+scanf ("%d", &x);
+if(x < 0 || n < 0)
+{
+printf (" illegal value ");
+}
+else
+{
+for (i = 0; i <= n; i ++)
+sum = sum + pow(x, i);
+}
+printf ("sum =%d", sum );
+return 0;
+}
